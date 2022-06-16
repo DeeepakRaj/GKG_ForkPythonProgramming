306724d14c3cf556a786dd8d9e6db4f2a8fa0a93
Deepakkumar Kantilal Raj <deeepak9018@gmail.com>
2022-06-17 00:03:10 +0530

    new folder added

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,1 +1,19 @@
+#Hello message
 print("hello")
+
+#Variables 
+myNum = 10
+print(myNum)
+
+myNum1 = 10.40
+print(myNum1)
+
+#Creating list
+nums = []
+
+nums.append(5)
+nums.append("Hello")
+nums.append(10.5)
+
+print(nums)
+
