703eab00fe1c7b4ca2efa9135d583f2dd726cc81
Deepakkumar Kantilal Raj <deeepak9018@gmail.com>
2022-06-18 23:35:49 +0530

    functions code added

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,19 +1,51 @@
 #Hello message
-print("hello")
+#print("hello")
 
-#Variables 
-myNum = 10
-print(myNum)
+#Variables {
+# myNum = 10
+# print(myNum)
 
-myNum1 = 10.40
-print(myNum1)
+# myNum1 = 10.40
+# print(myNum1)
+    
+#}
 
 #Creating list
-nums = []
+# nums = []
 
-nums.append(5)
-nums.append("Hello")
-nums.append(10.5)
+# nums.append(5)
+# nums.append("Hello")
+# nums.append(10.5)
 
-print(nums)
+# print(nums)
 
+# # Python program to illustrate
+# # functions
+# def hello():
+#     print("hello")
+#     print("hello again")
+# hello()
+
+# # calling function
+# hello() 
+
+
+
+# Python program to illustrate 
+# function with main
+def getInteger():
+    result = int(input("Enter integer: "))
+    return result
+
+def Main():
+    print("Started")
+
+    # calling the getInteger function and 
+    # storing its returned value in the output variable
+    output = getInteger()     
+    print(output)
+
+# now we are required to tell Python 
+# for 'Main' function existence
+if __name__=="__main__":
+    Main()
