61f9a003b0c6d4c128dcfdda264e029a69ec05a6
Deepakkumar Kantilal Raj <deeepak9018@gmail.com>
2022-06-20 17:09:47 +0530

    Somewhere between Basics of Python

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,51 +1,88 @@
-#Hello message
-#print("hello")
+# Introduction to python
+  # Hello message
+  # print("hello")
 
-#Variables {
-# myNum = 10
-# print(myNum)
+  # Variables {
+  # myNum = 10
+  # print(myNum)
 
-# myNum1 = 10.40
-# print(myNum1)
+  # myNum1 = 10.40
+  # print(myNum1)
+
+  # }
+
+  # Creating list
+  # nums = []
+
+  # nums.append(5)
+  # nums.append("Hello")
+  # nums.append(10.5)
+  # print(nums)
+
+  # # Python program to illustrate
+  # # functions
+  # def hello():
+  #     print("hello")
+  #     print("hello again")
+  # hello()
+
+  # # calling function
+   # hello()
+
+   # Python program to illustrate
+  # function with main
+#   from unittest import result
+
+#    def getInteger():
+#         result = int(input("Enter integer: "))
+#         # result = 10;
+#         return result
+
+#     def Main():
+#         print("Started")
+
+#         # calling the getInteger function and
+#         # storing its returned value in the output variable
+#         output = getInteger()
+#         print(output)
+
+#     # now we are required to tell Python
+#     # for 'Main' function existence
+#     if __name__ == "__main__":
+#         Main()
+
+#====================================================================
+# Basics of Python
+#if else condition
+from unittest import result
+
+# def getInteger():
+# a = input("Enter value of A: ")
+# b = input("Enter  value of B: ")
+
+a = int(input("Enter Value of A: "))
+b = int(input("Enter Value of B: "))
+    # return a,b
+
+    # if b > a == 0:
+    #     print("B is greater")
+    # else: 
+    #     print("A is greater")
     
-#}
-
-#Creating list
-# nums = []
-
-# nums.append(5)
-# nums.append("Hello")
-# nums.append(10.5)
-
-# print(nums)
-
-# # Python program to illustrate
-# # functions
-# def hello():
-#     print("hello")
-#     print("hello again")
-# hello()
-
-# # calling function
-# hello() 
+if b % a == 0 :
+    print ("b is divisible by a")
+elif b + 1 == 10:
+    print ("Increment in b produces 10")
+else:
+    print ("You are in else statement")
 
 
-
-# Python program to illustrate 
-# function with main
-def getInteger():
-    result = int(input("Enter integer: "))
-    return result
-
-def Main():
-    print("Started")
-
-    # calling the getInteger function and 
-    # storing its returned value in the output variable
-    output = getInteger()     
-    print(output)
-
-# now we are required to tell Python 
-# for 'Main' function existence
-if __name__=="__main__":
-    Main()
+# def Main():
+#     print("Starting program")
+#     output  = getInteger()
+#     print("Value of A & B")
+#     print(output)
+    
+#     # show = getInteger()
+# if __name__ == "__main__":
+#      Main()
