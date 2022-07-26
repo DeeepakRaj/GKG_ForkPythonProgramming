c0ecc803a6e8158986241862e2ba708204fc1b70
Deeepak Raj <deeepakraj@192.168.1.2>
2022-07-26 22:03:55 +0530

    Completed Loops

--- Module 2 : Control Statements and Loops/ControlFlow.py
+++ Module 2 : Control Statements and Loops/ControlFlow.py
@@ -17,17 +17,38 @@
 
 # Example 2: Python if else in list comprehension
 # Explicit function
-def digitSum(n):
-    dsum = 0
-    for ele in str(n):
-        dsum += int(ele)
-    return dsum
+# def digitSum(n):
+#     dsum = 0
+#     for ele in str(n):
+#         dsum += int(ele)
+#     return dsum
 
 
-# Initializing list
-List = [367, 111, 562, 945, 6726, 873]
+# # Initializing list
+# List = [367, 111, 562, 945, 6726, 873]
 
-# Using the function on odd elements of the list
-newList = [digitSum(i) for i in List if i & 1]
-# Displaying new list
-print(newList)
+# # Using the function on odd elements of the list
+# newList = [digitSum(i) for i in List if i & 1]
+# # Displaying new list
+# print(newList)
+
+#shorthand if else
+# i = 10
+# print("I is greater then 15") if i > 15 else print("I is less then 15")
+
+#Loops
+#While Loop
+# count = 0
+# while count <= 3:
+# #  print("Hello Deepak")
+#  count = count + 1
+#  print("Hello Deepak")
+#  print("Value of Count : ",count)
+
+
+#range
+i = 4
+for i in range(4,i*2+1):
+ print("Value of I : ",i)
+ 
+ #Completed Loops
